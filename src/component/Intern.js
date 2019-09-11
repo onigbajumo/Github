85d05abc575598352846cf80939e859c3fea3b42
onigbajumo <onigbajumogbenga@gmail.com>
2019-09-11 10:51:37 +0100

    my first commit

---
+++ src/component/Intern.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import '../cssfile/Intern.css'
+class Intern extends Component{
+    render(){
+        return (
+            <h1> Welcome to my first Todo App</h1>
+        )
+    }
+}
+export default Intern;
